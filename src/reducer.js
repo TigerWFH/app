5f36f80baf8304f0f71ec4eccd0c06c9809b7ffc
wangfanghua <wangfanghua@jk.cn>
2017-08-25 19:06:49 +0800

    异步注入reducers

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,17 @@
 import { combineReducers } from 'redux';
 
-import demoReducers from './pages/demo/reducers';
-import firstReducers from './pages/first/reducers';
-
-// export default (state = {}, action) => {
-//     switch (action.type) {
-//         case "TEST":
-//         default:
-//             return state;
-//     }
-// }
+import demo from './pages/demo/reducers';
+import first from './pages/first/reducers';
 
 export default combineReducers({
-    demo: demoReducers,
-    first: firstReducers
+    demo,
+    first
 });
+
+// export default createReducers = (asyncReducers) => {
+//     return combineReducers({
+//         ...asyncReducers,//异步载入的
+//         demo,
+//         first
+//     });
+// };
