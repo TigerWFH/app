a2d6be2a31cf5973c91da1247605c301261d34df
wangfanghua <wangfanghua@jk.cn>
2017-08-25 15:32:52 +0800

    异步路由数据接通

--- src/pages/first/container/index.jsx
+++ src/pages/first/container/index.jsx
@@ -1,13 +1,32 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import * as Actions from '../actions';
 
+function mapStateToProps(state, ownProps) {
+    let { first } = state;
+    return first;
+}
+function mapDispatchToProps(dispatch, ownProps) {
+    return {
+        getFirstData: () => dispatch(Actions.getFirstData())
+    };
+}
 class First extends React.Component {
+    _onGetData = () => {
+        let { getFirstData } = this.props;
+        getFirstData();
+    }
     render() {
+        let { data } = this.props;
         return (
             <div>
-                this is First World!
+                <button onClick={this._onGetData}>
+                    getFirstData
+                </button>
+                {'this is First World!' + data}
             </div>
         )
     }
 }
 
-export default First;
+export default connect(mapStateToProps, mapDispatchToProps, null)(First);
