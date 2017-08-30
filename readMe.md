cc1ce30253b436e2a0319933ff120a91aa714eb1
wangfanghua <wangfanghua@jk.cn>
2017-08-30 19:04:44 +0800

    验证了HTMLElement.scroll系列属性

--- readMe.md
+++ readMe.md
@@ -15,4 +15,25 @@
     reducers直接配置到rootReducer，并未进行code splitting，只有UI部分进行了 code splitting。
     
     思考：如何将对应的reducers进行code splitting。
-## 
+## 元素的宽高
+* client***系列：`content+padding+css width(标准盒模型情况下)`
+
+        clientWidth：
+        clientHeight：
+        clientTop：
+        clientLeft：the width of the left border of an element in pixels
+* offset***系列：`content+padding+scrollbar+css width（标准盒模型情况下）`
+
+        offsetWidth：HTMLElement.offWidth read-only property is a measurement which includes the element borders,horizontal padding,the element vertical scrollbar(if present,if rendered) and the css width.
+        offsetHeight：HTMLElement.offsetHeight read-only property is the height of the element including vertical padding,borders and the element horizontal scrollbar(if present,if rendered), as an integer.
+        offsetParent：positioned：返回最近定位元素的引用；non-positioned： 返回table,table-cell,html(标准模式),body(怪异模式),null(display:none)
+        offsetTop：返回当前元素相对于offsetParent顶部的距离
+        offsetLeft：返回当前元素相对于offsetParent左边的距离
+* scroll***系列：`is a measurement of the height of an elements content including content not visible on the screen due to overflow`
+
+        scrollWidth：
+        scrollHeight：
+        scrollTop：
+        scrollLeft：
+* getBoundingClientRect()
+* getClientRect()
--- src/pages/demo/container/index.jsx
+++ src/pages/demo/container/index.jsx
@@ -11,8 +11,9 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as Actions from '../actions';
-
+import * as css from './index.less';
 import View from '../../../common/widgets/view';
+
 function mapPropsToState(state, ownProps) {
   let { demo } = state;
   return { ...demo };
@@ -28,19 +29,40 @@ class Demo extends React.Component {
   static defaultProps = {//会和Provider第一次传进来的数据合并之后，进行第一次渲染：provider提供默认值，使用provider提供的默认值；否者使用class自定义的
     data: "default"
   }
-  _onSendTest = () => {
+  _onSendTest = (e) => {
     let { sendTest } = this.props;
+    e.stopPropagation();
+    alert('button')
     sendTest();
   }
 
+  _onScrollF = (e) => {
+    e.preventDefault();
+  }
+
+  _onScroll(e) {
+    e.stopPropagation();
+  }
   render() {
     let { data } = this.props;
     return (
       <View>
-        <button onClick={this._onSendTest}>
-          sendTest
-        </button>
-        {'this is Demo world! +' + data}
+        <div onScroll={this._onScrollF}
+          className={css["scrollWrapper"]}>
+          滚动askadfkafkaskllkfjlsafjlsakflsadkfsakfksafjasd
+          <div onScroll={this._onScroll}
+            className={css["scroll"]}>
+            child contentaasfdsadfasfasfasfasfdsaf
+          </div>
+        </div>
+        <div onClick={() => {
+          alert(); 123
+        }}>
+          <button onClick={this._onSendTest}>
+            sendTest
+          </button>
+          {'this is Demo world! +' + data}
+        </div>
       </View>
     )
   }
