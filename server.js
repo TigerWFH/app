702c198182b6079c38a9d8ebdeee6f49a75dcae7
monkey-ubuntu <fanghuawfh@gmail.com>
2017-10-22 12:22:11 +0800

    添加mock server

---
+++ server.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+let express = require('express');
+var app = express();
+
+var crypto = require('crypto');
+var querystring = require("querystring");
+var multer = require('multer');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+app.post('/v1/upload', multer({ dest: 'upload/' }).array('video', 2), (req, res, next) => {
+	console.log('req.body--->', req.body);
+	console.log('req.file--->', req.file);
+});
+app.post('/v1/signon', (req, res, next) => {
+	res.status(200).json({
+		name: "signon",
+		age: 12
+	});
+});
+app.get('/v1/signon', (req, res, next)=>{
+    res.status(200).json({
+        name: 'test get',
+        age: 12
+    })
+})
+app.all('*', (req, res, next) => {
+	console.log('***I am a APIServer***', req.path);
+	var filePath = `./mock/${req.path}/index.json`;
+	if (fs.existsSync(filePath)) {
+		let data = fs.readFileSync(filePath, { encoding: 'utf8' });
+		console.log('data-->', JSON.parse(data));
+		res.json(JSON.parse(data));
+	}
+	else {
+		next();
+	}
+});
+
+// app.use(express.static('./dist'));
+// app.get('/', (req, res) => {
+// 	res.sendfile('./dist/index.html');
+// });
+
+// app.post('/v1/signon', (req, res) => {
+// 	res.json({
+// 		msgCode: 0,
+// 		msgText: 'success',
+// 		login: true
+// 	});
+// });
+app.use(function (req, res, next) {
+	var err = new Error('NOT FOUND');
+	err.status = 404;
+	next(err);
+})
+if (app.get('env') === 'development') {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.json(err);
+	});
+}
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.json(err);
+});
+app.listen(9000, function () {
+	console.log('App listening on port 9000!');
+});
